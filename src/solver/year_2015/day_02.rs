6aeccfcada6ae558f4c819329a9f02ea0fc5f05b
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-07-25 13:40:19 +0800

    Solved 2015 02_2

--- src/solver/year_2015/day_02.rs
+++ src/solver/year_2015/day_02.rs
@@ -4,7 +4,7 @@ pub const SOLVER: Solver = Solver {
     year: 2015,
     day: 2,
     title: "I Was Told There Would Be No Math",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
@@ -28,6 +28,29 @@ fn solve_1(input: &str) -> Solution {
     Solution::U32(total_wrapping_paper)
 }
 
+fn solve_2(input: &str) -> Solution {
+    let mut total_wrapping_paper = 0;
+    for line in input.lines() {
+        let dimensions = line
+            .split('x')
+            .map(|num| {
+                num.parse::<u32>()
+                    .expect("All dimensions should be numbers")
+            })
+            .collect::<Vec<u32>>();
+        // To prevent needless doubling, half of the perimeter is calculated here and the smallest
+        // value is later doubled when adding to total_wrapping_paper.
+        let perimeters = [
+            dimensions[0] + dimensions[1],
+            dimensions[0] + dimensions[2],
+            dimensions[1] + dimensions[2],
+        ];
+        total_wrapping_paper += 2 * perimeters.iter().min().expect("Should have sides")
+            + dimensions.iter().product::<u32>();
+    }
+    Solution::U32(total_wrapping_paper)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -39,5 +62,14 @@ mod test {
     #[test]
     fn example1_2() {
         assert_eq!(solve_1("1x1x10"), Solution::U8(43));
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(solve_2("2x3x4"), Solution::U8(34));
+    }
+    #[test]
+    fn example2_2() {
+        assert_eq!(solve_2("1x1x10"), Solution::U8(14));
     }
 }
