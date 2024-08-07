d1ba87a39a0c0de8d6d2e6b39ae5e247558f1c3d
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-08-07 22:31:57 +0800

    Solved 2015 05_2

--- src/solver/year_2015/day_05.rs
+++ src/solver/year_2015/day_05.rs
@@ -1,10 +1,11 @@
 use crate::solver::{Solution, Solver};
+use itertools::Itertools;
 
 pub const SOLVER: Solver = Solver {
     year: 2015,
     day: 5,
     title: "Doesn't He Have Intern-Elves For This?",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
@@ -50,6 +51,50 @@ fn solve_1(input: &str) -> Solution {
     Solution::U32(nice_strings)
 }
 
+fn solve_2(input: &str) -> Solution {
+    let mut nice_strings = 0;
+
+    for line in input.lines() {
+        let characters = line.chars().collect::<Vec<_>>();
+
+        let mut has_repeat_after_any = false;
+        let mut has_repeated_pair = false;
+
+        let mut char_iter = characters.iter().multipeek();
+        while let Some(character) = char_iter.next() {
+            if let Some(_middle_character) = char_iter.peek() {
+                if let Some(character_after_any) = char_iter.peek() {
+                    if character == *character_after_any {
+                        has_repeat_after_any = true;
+                    }
+                }
+            }
+        }
+
+        let mut character_pairs = characters.windows(2);
+        while let Some(first_character_pair) = character_pairs.next() {
+            // Cloning the iterator makes it possible to iterate from the current element to the end
+            // searching for matches, while leaving the original iterator untouched so this search
+            // can be repeated from the next element.
+            let mut character_pairs_clone = character_pairs.clone();
+            // Immediately call next() once and ignore the value to avoid counting matches that
+            // overlap, such as "aaa".
+            character_pairs_clone.next();
+            for second_character_pair in character_pairs_clone {
+                if first_character_pair == second_character_pair {
+                    has_repeated_pair = true;
+                }
+            }
+        }
+
+        if has_repeat_after_any && has_repeated_pair {
+            nice_strings += 1;
+        }
+    }
+
+    Solution::U32(nice_strings)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -73,5 +118,22 @@ mod test {
     #[test]
     fn example1_5() {
         assert_eq!(solve_1("dvszwmarrgswjxmb"), Solution::U8(0));
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(solve_2("qjhvhtzxzqqjkmpb"), Solution::U8(1));
+    }
+    #[test]
+    fn example2_2() {
+        assert_eq!(solve_2("xxyxx"), Solution::U8(1));
+    }
+    #[test]
+    fn example2_3() {
+        assert_eq!(solve_2("uurcxstgmygtbstg"), Solution::U8(0));
+    }
+    #[test]
+    fn example2_4() {
+        assert_eq!(solve_2("ieodomkazucvgmuy"), Solution::U8(0));
     }
 }
