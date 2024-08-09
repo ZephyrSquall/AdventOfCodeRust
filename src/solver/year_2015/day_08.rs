fd4ca9e22c57ab46fe9b5b62e3a58e7e3c9908b5
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-08-09 18:30:13 +0800

    Solved 2015 08_1

---
+++ src/solver/year_2015/day_08.rs
@@ -0,0 +1,72 @@
+use crate::solver::{Solution, Solver};
+
+pub const SOLVER: Solver = Solver {
+    year: 2015,
+    day: 8,
+    title: "Matchsticks",
+    part_solvers: &[solve_1],
+};
+
+fn solve_1(input: &str) -> Solution {
+    // Tracking the absolute number of characters in code and characters in memory is unnecessary,
+    // only the difference between them matters.
+    let mut character_difference = 0;
+
+    for line in input.lines() {
+        // Always add 2 to character_difference for each line to represent the difference caused by
+        // the opening and closing quotation marks.
+        character_difference += 2;
+        let mut is_escaped = false;
+
+        for character in line.chars() {
+            if is_escaped {
+                match character {
+                    // \" and \\ are both 2 characters in code and 1 character in memory, for a
+                    // difference of 1.
+                    '\"' | '\\' => character_difference += 1,
+                    // \x00 ("00" is any two-digit hexadecimal number) is 4 characters in code and 1
+                    // character in memory, for a difference of 3. A hexadecimal number can never
+                    // include a back-slash, so it's safe to set is_escaped back to false even
+                    // though we've only reached the 'x' character in the string so far.
+                    'x' => character_difference += 3,
+                    _ => panic!("Unrecognized escape sequence"),
+                }
+                is_escaped = false;
+            } else {
+                if character == '\\' {
+                    is_escaped = true;
+                }
+                // If a character is not escaped, then it is 1 character in code and 1 character in
+                // memory, for a difference of 0.
+            }
+        }
+    }
+
+    Solution::U32(character_difference)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1_1() {
+        // Input string is: ""
+        assert_eq!(solve_1("\"\""), Solution::U8(2));
+    }
+    #[test]
+    fn example1_2() {
+        // Input string is: "abc"
+        assert_eq!(solve_1("\"abc\""), Solution::U8(2));
+    }
+    #[test]
+    fn example1_3() {
+        // Input string is: "aaa\"aaa"
+        assert_eq!(solve_1("\"aaa\\\"aaa\""), Solution::U8(3));
+    }
+    #[test]
+    fn example1_4() {
+        // Input string is: "\x27"
+        assert_eq!(solve_1("\"\\x27\""), Solution::U8(5));
+    }
+}
