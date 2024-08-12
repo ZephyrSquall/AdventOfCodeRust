d5fd01869b18c23fa7216f42637de94f8e8d33fd
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-08-12 12:58:15 +0800

    Solved 2015 14_2

--- src/solver/year_2015/day_14.rs
+++ src/solver/year_2015/day_14.rs
@@ -4,37 +4,48 @@ pub const SOLVER: Solver = Solver {
     year: 2015,
     day: 14,
     title: "Reindeer Olympics",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
-    solve(input, 2503)
+    solve(input, 2503, false)
 }
 
-fn solve(input: &str, race_time: u32) -> Solution {
+fn solve_2(input: &str) -> Solution {
+    solve(input, 2503, true)
+}
+
+fn solve(input: &str, race_time: u32, is_using_points: bool) -> Solution {
     let mut reindeers = get_reindeers(input);
     race(&mut reindeers, race_time);
 
     let winning_distance = reindeers
         .iter()
-        .map(|reindeer| reindeer.distance)
+        .map(|reindeer| {
+            if is_using_points {
+                reindeer.points
+            } else {
+                reindeer.distance
+            }
+        })
         .max()
         .expect("Reindeers shouldn't be empty");
     Solution::U32(winning_distance)
 }
 
-// We don't care who the winning reindeer is, only the winner's distance, so reindeer names aren't
-// required.
+// We don't care who the winning reindeer is, only the winner's distance/points, so reindeer names
+// aren't required.
 struct Reindeer {
     // speed, flight_time, and rest_time define the reindeer's attributes. They must be initialized
     // ahead of time, and don't change later.
     speed: u32,
     flight_time: u32,
     rest_time: u32,
-    // distance, is_resting, and timer track the reindeer's state during the race. They are
+    // distance, points, is_resting, and timer track the reindeer's state during the race. They are
     // automatically initialized to the same default values for all reindeer, and mutated during the
     // race.
     distance: u32,
+    points: u32,
     is_resting: bool,
     timer: u32,
 }
@@ -45,6 +56,7 @@ impl Reindeer {
             flight_time,
             rest_time,
             distance: 0,
+            points: 0,
             is_resting: false,
             timer: 0,
         }
@@ -96,9 +108,10 @@ fn get_reindeers(input: &str) -> Vec<Reindeer> {
     reindeers
 }
 
-// Run the race. Each reindeer's distance is stored in its distance field.
+// Run the race. Each reindeer's final distance and points are stored in their corresponding fields.
 fn race(reindeers: &mut [Reindeer], race_time: u32) {
     for _ in 0..race_time {
+        let mut max_distance = 0;
         for reindeer in reindeers.iter_mut() {
             reindeer.timer += 1;
 
@@ -114,6 +127,17 @@ fn race(reindeers: &mut [Reindeer], race_time: u32) {
                     reindeer.timer = 0;
                 }
             }
+
+            if reindeer.distance > max_distance {
+                max_distance = reindeer.distance;
+            }
+        }
+
+        // After all reindeer have moved in the current time step, award points.
+        for reindeer in reindeers.iter_mut() {
+            if reindeer.distance == max_distance {
+                reindeer.points += 1;
+            }
         }
     }
 }
@@ -127,11 +151,26 @@ mod test {
         assert_eq!(
             solve(
                 "\
-        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
-        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
-                1000
+Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                1000,
+                false
             ),
             Solution::U16(1120)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve(
+                "\
+Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                1000,
+                true
+            ),
+            Solution::U16(689)
         );
     }
 }
