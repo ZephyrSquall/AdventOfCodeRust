207030ad9a1b42f6399452b7d65a1cb0cd928db1
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-06 22:44:18 +0800

    Solved 2015 24_2

--- src/solver/year_2015/day_24.rs
+++ src/solver/year_2015/day_24.rs
@@ -6,18 +6,11 @@ pub const SOLVER: Solver = Solver {
     year: 2015,
     day: 24,
     title: "It Hangs in the Balance",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
-    let mut package_weights = Vec::new();
-    let mut package_weights_sum = 0;
-
-    for line in input.lines() {
-        let package_weight = line.parse().expect("Line should contain a single number");
-        package_weights.push(package_weight);
-        package_weights_sum += package_weight;
-    }
+    let (package_weights, package_weights_sum) = get_package_weights_and_sum(input);
 
     // Since the total weight never changes and the weight is divided evenly among the three groups,
     // the total weight of each group is always exactly a third of the total weight of all packages.
@@ -89,6 +82,87 @@ fn solve_1(input: &str) -> Solution {
     Solution::U64(minimum_quantum_entanglement)
 }
 
+fn solve_2(input: &str) -> Solution {
+    let (package_weights, package_weights_sum) = get_package_weights_and_sum(input);
+
+    // This time it is assumed the sum of all package weights is divisible by 4.
+    let group_weight = package_weights_sum / 4;
+
+    let mut minimum_quantum_entanglement = u64::MAX;
+
+    // The same strategy as before can be used, however the search now needs to go down to the third
+    // group to be certain valid groups exist (once the first three valid groups are found, the
+    // remaining packages must form the fourth valid group).
+    for first_group_size in 1..(package_weights.len() - 3) {
+        let mut has_found_valid_group = false;
+        'first_group: for first_group in package_weights.iter().combinations(first_group_size) {
+            if first_group.iter().copied().sum::<u64>() == group_weight {
+                // First valid group found, check for a second valid group.
+
+                let mut first_remaining_package_weights = package_weights.clone();
+                first_remaining_package_weights
+                    .retain(|package_weight| !first_group.contains(&package_weight));
+
+                for second_group_size in 1..(first_remaining_package_weights.len() - 2) {
+                    for second_group in first_remaining_package_weights
+                        .iter()
+                        .combinations(second_group_size)
+                    {
+                        if second_group.iter().copied().sum::<u64>() == group_weight {
+                            // Second valid group found, check for a third valid group.
+
+                            let mut second_remaining_package_weights =
+                                first_remaining_package_weights.clone();
+                            second_remaining_package_weights
+                                .retain(|package_weight| !second_group.contains(&package_weight));
+
+                            for third_group_size in 1..(second_remaining_package_weights.len() - 1)
+                            {
+                                for third_group in second_remaining_package_weights
+                                    .iter()
+                                    .combinations(third_group_size)
+                                {
+                                    if third_group.iter().copied().sum::<u64>() == group_weight {
+                                        // Third valid group found, so this is overall a valid
+                                        // configuration. Update minimum_quantum_entanglement
+                                        // accordingly.
+                                        minimum_quantum_entanglement = min(
+                                            minimum_quantum_entanglement,
+                                            first_group.iter().copied().product(),
+                                        );
+                                        has_found_valid_group = true;
+
+                                        continue 'first_group;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if has_found_valid_group {
+            break;
+        }
+    }
+
+    Solution::U64(minimum_quantum_entanglement)
+}
+
+fn get_package_weights_and_sum(input: &str) -> (Vec<u64>, u64) {
+    let mut package_weights = Vec::new();
+    let mut package_weights_sum = 0;
+
+    for line in input.lines() {
+        let package_weight = line.parse().expect("Line should contain a single number");
+        package_weights.push(package_weight);
+        package_weights_sum += package_weight;
+    }
+
+    (package_weights, package_weights_sum)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -110,6 +184,26 @@ mod test {
 11"
             ),
             Solution::U8(99)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+1
+2
+3
+4
+5
+7
+8
+9
+10
+11"
+            ),
+            Solution::U8(44)
         );
     }
 }
