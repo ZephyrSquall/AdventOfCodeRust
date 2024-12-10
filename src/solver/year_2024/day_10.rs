38202d76a364ed77d2697733d0215904a3430086
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-10 13:42:23 +0800

    Solved 2024 10_2

--- src/solver/year_2024/day_10.rs
+++ src/solver/year_2024/day_10.rs
@@ -5,16 +5,10 @@ pub const SOLVER: Solver = Solver {
     year: 2024,
     day: 10,
     title: "Hoof It",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
-    #[derive(PartialEq, Eq, Hash)]
-    struct Position {
-        x: usize,
-        y: usize,
-    }
-
     // A recursive function that keeps searching positions directly adjacent to the current position
     // to check if their height is one greater. When a height of 9 is found, the position is
     // inserted into a hash set of found positions at that height (hash sets ensure no duplicates)
@@ -71,23 +65,10 @@ fn solve_1(input: &str) -> Solution {
         }
     }
 
-    // Build the topographic map.
-    let mut topographic_map = Vec::new();
-    for line in input.lines() {
-        let mut topographic_map_line = Vec::new();
-        for digit in line.chars().map(|character| {
-            character
-                .to_digit(10)
-                .expect("Every character should be a digit from 0 to 9")
-        }) {
-            topographic_map_line.push(digit);
-        }
-        topographic_map.push(topographic_map_line);
-    }
-
     // Search the topographic map for trailheads (heights of 0). When one is found, call
     // find_hiking_trails to get all 9-height positions reachable from it, and add the number of
     // them to the score.
+    let topographic_map = get_topographic_map(input);
     let mut score = 0;
     for (y, topographic_map_line) in topographic_map.iter().enumerate() {
         for (x, height) in topographic_map_line.iter().enumerate() {
@@ -105,6 +86,97 @@ fn solve_1(input: &str) -> Solution {
     }
 
     Solution::USize(score)
+}
+
+fn solve_2(input: &str) -> Solution {
+    // A recursive function that keeps searching positions directly adjacent to the current position
+    // to check if their height is one greater. When a height of 9 is found, the score is
+    // incremented by 1.
+    fn find_hiking_trails(
+        topographic_map: &[Vec<u32>],
+        position: &Position,
+        current_height: u32,
+    ) -> u32 {
+        let mut next_positions = Vec::with_capacity(4);
+        if position.x > 0 {
+            next_positions.push(Position {
+                x: position.x - 1,
+                y: position.y,
+            });
+        }
+        if position.y > 0 {
+            next_positions.push(Position {
+                x: position.x,
+                y: position.y - 1,
+            });
+        }
+        if position.x + 1 < topographic_map[0].len() {
+            next_positions.push(Position {
+                x: position.x + 1,
+                y: position.y,
+            });
+        }
+        if position.y + 1 < topographic_map.len() {
+            next_positions.push(Position {
+                x: position.x,
+                y: position.y + 1,
+            });
+        }
+
+        let mut score = 0;
+        let next_height = current_height + 1;
+        if next_height == 9 {
+            for next_position in next_positions {
+                if topographic_map[next_position.y][next_position.x] == 9 {
+                    score += 1;
+                }
+            }
+        } else {
+            for next_position in next_positions {
+                if topographic_map[next_position.y][next_position.x] == next_height {
+                    score += find_hiking_trails(topographic_map, &next_position, next_height);
+                }
+            }
+        }
+        score
+    }
+
+    // Search the topographic map for trailheads (heights of 0). When one is found, call
+    // find_hiking_trails to get a count of all distinct hiking trails from it, and add the number
+    // of them to the score.
+    let topographic_map = get_topographic_map(input);
+    let mut score = 0;
+    for (y, topographic_map_line) in topographic_map.iter().enumerate() {
+        for (x, height) in topographic_map_line.iter().enumerate() {
+            if *height == 0 {
+                score += find_hiking_trails(&topographic_map, &Position { x, y }, 0);
+            }
+        }
+    }
+
+    Solution::U32(score)
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+fn get_topographic_map(input: &str) -> Vec<Vec<u32>> {
+    let mut topographic_map = Vec::new();
+    for line in input.lines() {
+        let mut topographic_map_line = Vec::new();
+        for digit in line.chars().map(|character| {
+            character
+                .to_digit(10)
+                .expect("Every character should be a digit from 0 to 9")
+        }) {
+            topographic_map_line.push(digit);
+        }
+        topographic_map.push(topographic_map_line);
+    }
+    topographic_map
 }
 
 #[cfg(test)]
@@ -126,6 +198,24 @@ mod test {
 10456732"
             ),
             Solution::U8(36)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"
+            ),
+            Solution::U8(81)
         );
     }
 }
