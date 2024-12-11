d8463a9614173aef4036e718c6deedab8a69b7c5
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-11 11:10:11 +0800

    Solved 2016 02_2

--- src/solver/year_2016/day_02.rs
+++ src/solver/year_2016/day_02.rs
@@ -4,7 +4,7 @@ pub const SOLVER: Solver = Solver {
     year: 2016,
     day: 2,
     title: "Bathroom Security",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
@@ -46,6 +46,66 @@ fn solve_1(input: &str) -> Solution {
     Solution::String(code.into_iter().collect())
 }
 
+fn solve_2(input: &str) -> Solution {
+    // None is used to represent space that can't be moved into. The whole keypad is surrounded by
+    // edges of None so no other bound checking is required.
+    const KEYPAD: [[Option<char>; 7]; 7] = [
+        [None, None, None, None, None, None, None],
+        [None, None, None, Some('1'), None, None, None],
+        [None, None, Some('2'), Some('3'), Some('4'), None, None],
+        [
+            None,
+            Some('5'),
+            Some('6'),
+            Some('7'),
+            Some('8'),
+            Some('9'),
+            None,
+        ],
+        [None, None, Some('A'), Some('B'), Some('C'), None, None],
+        [None, None, None, Some('D'), None, None, None],
+        [None, None, None, None, None, None, None],
+    ];
+
+    let mut code = Vec::new();
+    // KEYPAD[3][1] is the '5', the starting position for the first line.
+    let mut x: usize = 1;
+    let mut y: usize = 3;
+    for line in input.lines() {
+        for direction in line.chars() {
+            let x_next;
+            let y_next;
+            match direction {
+                'U' => {
+                    x_next = x;
+                    y_next = y - 1;
+                }
+                'R' => {
+                    x_next = x + 1;
+                    y_next = y;
+                }
+                'D' => {
+                    x_next = x;
+                    y_next = y + 1;
+                }
+                'L' => {
+                    x_next = x - 1;
+                    y_next = y;
+                }
+                _ => panic!("Unsupported direction"),
+            }
+
+            if KEYPAD[y_next][x_next].is_some() {
+                x = x_next;
+                y = y_next;
+            }
+        }
+        code.push(KEYPAD[y][x].expect("The keypad position should never move onto a None"));
+    }
+
+    Solution::String(code.into_iter().collect())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -61,6 +121,20 @@ LURDL
 UUUUD"
             ),
             Solution::U16(1985)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+ULL
+RRDDD
+LURDL
+UUUUD"
+            ),
+            Solution::Str("5DB3")
         );
     }
 }
