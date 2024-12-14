0144b6a8b0829afe0199c4a9c1eca9aa603fe63b
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-14 10:04:51 +0800

    Solved 2016 04_2

--- src/solver/year_2016/day_04.rs
+++ src/solver/year_2016/day_04.rs
@@ -5,7 +5,7 @@ pub const SOLVER: Solver = Solver {
     year: 2016,
     day: 4,
     title: "Security Through Obscurity",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
@@ -13,7 +13,7 @@ fn solve_1(input: &str) -> Solution {
 
     for line in input.lines() {
         let mut letter_counts = FxHashMap::default();
-        let mut sector_id_digits = Vec::default();
+        let mut sector_id_digits = Vec::new();
 
         let mut character_iter = line.chars();
         // Loop over all characters until the checksum, indicated by the '[' character.
@@ -82,6 +82,103 @@ fn solve_1(input: &str) -> Solution {
         }
     }
     Solution::U32(sector_id_sum)
+}
+
+// Assume that the room we're looking for is the only room that contains the words "north-pole".
+fn solve_2(input: &str) -> Solution {
+    for line in input.lines() {
+        let mut message = Vec::new();
+        let mut letter_counts = FxHashMap::default();
+        let mut sector_id_digits = Vec::new();
+
+        let mut character_iter = line.chars();
+        loop {
+            let character = character_iter
+                .next()
+                .expect("Line should not end before the checksum is reached");
+            if character == '[' {
+                break;
+            }
+
+            if character == '-' {
+                // Insert dashes into the message
+                message.push(character);
+            } else {
+                if character.is_ascii_digit() {
+                    sector_id_digits.push(character);
+                    // Don't insert numbers into the message.
+                } else {
+                    letter_counts
+                        .entry(character)
+                        .and_modify(|count| *count += 1)
+                        .or_insert(1);
+                    // Insert letters into the message.
+                    message.push(character);
+                }
+            }
+        }
+
+        let mut letter_counts = letter_counts.into_iter().collect::<Vec<_>>();
+        letter_counts.sort_unstable_by(|a, b| {
+            if a.1 == b.1 {
+                return a.0.cmp(&b.0);
+            }
+            a.1.cmp(&b.1).reverse()
+        });
+
+        let mut is_checksum_valid = true;
+        for ordered_letter in letter_counts
+            .iter()
+            .take(5)
+            .map(|letter_count| letter_count.0)
+        {
+            if ordered_letter
+                != character_iter
+                    .next()
+                    .expect("Checksum should have 5 characters")
+            {
+                is_checksum_valid = false;
+                break;
+            }
+        }
+
+        // If the checksum is valid, shift the characters in the message by the sector id, then
+        // check if the room contains the north pole objects. The puzzle description isn't explicit
+        // about exactly how it is determined that a room contains the north pole objects, so I
+        // assume the puzzle input is structured so that this room is the only room in the puzzle
+        // input that contains the words "north" and "pole", i.e. I assume the room whose sector ID
+        // we want is the only room that contains the substrings "north" and "pole".
+        if is_checksum_valid {
+            let sector_id = sector_id_digits
+                .iter()
+                .collect::<String>()
+                .parse::<u32>()
+                .expect("Sector ID should be a number");
+
+            // Every 26 shifts puts a letter back to its original value, so only the remainder after
+            // the last multiple of 26 matters for shifting.
+            let sector_id_shift = (sector_id % 26) as u8;
+            for letter in &mut message {
+                if *letter != '-' {
+                    let mut letter_ascii = *letter as u8 + sector_id_shift;
+                    // 122 is the ascii value for 'z'. If this value is exceeded, subtract 26 to
+                    // wrap around to the start of the alphabet. It is assumed that all letters in
+                    // the input are lowercase.
+                    if letter_ascii > 122 {
+                        letter_ascii -= 26;
+                    }
+
+                    *letter = letter_ascii as char;
+                }
+            }
+
+            let message = message.into_iter().collect::<String>();
+            if message.contains("north") && message.contains("pole") {
+                return Solution::U32(sector_id);
+            }
+        }
+    }
+    panic!("Should have found the room with the North Pole objects")
 }
 
 #[cfg(test)]
