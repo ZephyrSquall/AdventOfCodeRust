13f5bfc813d46167ffc45ca43e2840b104c327a7
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-15 12:39:50 +0800

    Solved 2016 06_2

--- src/solver/year_2016/day_06.rs
+++ src/solver/year_2016/day_06.rs
@@ -1,14 +1,38 @@
 use crate::solver::{Solution, Solver};
 use rustc_hash::FxHashMap;
+use std::cmp::{max_by_key, min_by_key};
 
 pub const SOLVER: Solver = Solver {
     year: 2016,
     day: 6,
     title: "Signals and Noise",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
+// The only difference between part 1 and part 2 is whether we're looking for the letter with the
+// maximum or the minimum count. So create a generic solver that works with any given reducing
+// function (the closure provided to the .reduce() method of an iterator), and provide the specific
+// reducing function for each specific part solver. Letter counts come from a hash map of char keys
+// (the letter) to u32 values (how many of that letter was counted), so they have the type (char,
+// u32).
 fn solve_1(input: &str) -> Solution {
+    fn reducing_function(acc: (char, u32), letter_count: (char, u32)) -> (char, u32) {
+        max_by_key(acc, letter_count, |letter_count| letter_count.1)
+    }
+    solve(input, reducing_function)
+}
+
+fn solve_2(input: &str) -> Solution {
+    fn reducing_function(acc: (char, u32), letter_count: (char, u32)) -> (char, u32) {
+        min_by_key(acc, letter_count, |letter_count| letter_count.1)
+    }
+    solve(input, reducing_function)
+}
+
+fn solve<F: FnMut((char, u32), (char, u32)) -> (char, u32)>(
+    input: &str,
+    mut reducing_function: F,
+) -> Solution {
     // Check the length of the message, then set up a vector of hash maps to track how many times
     // each letter occurs in each position.
     let message_len = input
@@ -29,14 +53,14 @@ fn solve_1(input: &str) -> Solution {
         }
     }
 
-    // Map each hash map in letter_counts to its most frequent letter, then collect the letters into
-    // a String.
+    // Map each hash map in letter_counts to its most or least frequent letter according to the
+    // provided reducing function, then collect the letters into a String.
     let corrected_message = letter_counts
         .into_iter()
         .map(|letter_count_map| {
             letter_count_map
                 .into_iter()
-                .max_by_key(|letter_count| letter_count.1)
+                .reduce(&mut reducing_function)
                 .expect("Each letter position should have a count for at least one letter")
                 .0
         })
@@ -72,6 +96,32 @@ dvrsen
 enarar"
             ),
             Solution::Str("easter")
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar"
+            ),
+            Solution::Str("advent")
         );
     }
 }
