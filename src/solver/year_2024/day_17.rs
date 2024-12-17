9706fac072aabeb3351e6fc4b13baefd6cd6d7c0
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-17 20:29:20 +0800

    Solved 2024 17_2

--- src/solver/year_2024/day_17.rs
+++ src/solver/year_2024/day_17.rs
@@ -5,7 +5,7 @@ pub const SOLVER: Solver = Solver {
     year: 2024,
     day: 17,
     title: "Chronospatial Computer",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
@@ -193,12 +193,195 @@ fn solve_1(input: &str) -> Solution {
     Solution::String(output_string)
 }
 
+// Possible truncation happens in the exponents of the .pow() methods. However, the values there
+// (roughly the number of digits in another number) aren't close to the maximum value a u32 could
+// hold, so truncation won't happen.
+#[allow(clippy::cast_possible_truncation)]
+fn solve_2(input: &str) -> Solution {
+    // Solving this part in general for any possible input seems impractical and obviously not the
+    // intended solution. For my own puzzle input, I was able to determine the answer is at least
+    // 35,184,372,088,832 and at most 281,474,976,710,655 (8^15 and 8^16 - 1), which leaves
+    // 246,290,604,621,824 possible answers to check, an amount that will take at least several
+    // hours to search. As such, I have to take advantage of patterns I find in my puzzle input that
+    // weren't given in the puzzle description. I have no way to check if the patterns I take
+    // advantage of are present in all puzzle inputs, so it's possible this solver will give
+    // incorrect answers for other puzzle inputs.
+    //
+    // By converting the opcodes and operands in my puzzle inputs into their corresponding
+    // instructions, I find that the program in my puzzle input is equivalent to the following
+    // pseudocode (^ represents XOR):
+
+    // loop: {
+    //     b = a % 8;
+    //     b = b ^ 1;
+    //     c = a / 2.pow(b);
+    //     b = b ^ 5;
+    //     b = b ^ c;
+    //     outputs.push(b % 8);
+    //     a = a / 8;
+    //     if a == 0 {
+    //         break;
+    //     }
+    // }
+
+    // The initial values of registers b and c are irrelevant, because they are assigned to before
+    // they are first read. a is divided by 8 many times, so it is easier to think of a as an octal
+    // number (written in base 8). In octal form, the line "b = a % 8" sets b to the value of the
+    // last digit in a, the line "a = a / 8" removes the last digit in a, and the condition
+    // "if a == 0" checks if all digits in a have been removed. With this information, this
+    // pseudocode can be simplified to:
+
+    // while(a has digits): {
+    //     b = a_last_digit;
+    //     b = b ^ 1;
+    //     c = a / 2.pow(b);
+    //     b = b ^ 5;
+    //     b = b ^ c;
+    //     outputs.push(b % 8);
+    //     a.remove_last_digit()
+    // }
+
+    // We can use this pseudocode to conclude that on each iteration, the output digit that is
+    // pushed is given by `output = (((a_last_digit ^ 1) ^ 5) ^ (a / 2.pow(a_last_digit ^ 1))) % 8`.
+    // We know what each output digit needs to be, so in principle, we can work backwards from this
+    // to determine what a_last_digit must have been to produce that output digit.
+    //
+    // However, the fact that both a and a_last_digit change each loop iteration is problematic.
+    // Thought this can be resolved by flipping the logic and working backwards from the final
+    // output value. To ensure that the program doesn't loop again after the final output value, a
+    // must be on its last digit at this point, which means a and a_last_digit are equal. Once a
+    // value for a is determined for this loop, this "locks in" the first digit of a. Then the
+    // second-last value of the output can be considered, where we know that
+    // a = 8 * a_prev + a_digit (where a_prev is the value of a calculated for the final output
+    // digit). This strategy can be continued all the way down, where a_prev is the octal number
+    // formed by all digits of a found so far, multiplied by 8 (so the final digit is determined by
+    // a_last_digit, not a_prev).
+    //
+    // Working backwards like this is also convenient in that it helps find the minimum value
+    // easily. For each step, if there are multiple values of a_last_digit that solve the equation,
+    // we simply choose the smallest value. It's also possible there might be no values of
+    // a_last_digit that solve the equation, in which case an earlier value of a_last_digit isn't
+    // usable so go back to that value and continue checking greater numbers from there. As long as
+    // digits are checked in increasing order like this, the first full set of digits that solves
+    // the equation for every output value must be the digits that give the smallest overall number
+    // for a.
+
+    // This function recursively finds the octal digits of a, working backwards from the last output
+    // number.
+    fn get_a_octal_digits(
+        a_octal_digits: &mut Vec<u64>,
+        output: &Vec<u64>,
+        output_index: usize,
+        // For the very first digit, it must be forced to be 1 or greater, as if it were 0, a
+        // wouldn't have enough digits to loop enough times to produce all the output values. No
+        // other digit in a has this restriction, so they can be 0.
+        first_a_digit: u64,
+    ) -> bool {
+        let output_digit = output[output_index];
+        let a_prev = a_octal_digits
+            .iter()
+            .rev()
+            .enumerate()
+            // Get the value of the octal number by multiplying each digit by 8 to the power of that
+            // digit's position.
+            .fold(0, |acc, (place, digit)| {
+                acc + digit * 8_u64.pow(place as u32)
+            })
+            // Multiply 8 to shift all digits one position over, so that the last digit is
+            // determined by a_last_digit.
+            * 8;
+
+        for a_last_digit in first_a_digit..8 {
+            if output_digit
+                == (((a_last_digit ^ 1) ^ 5)
+                    ^ ((a_prev + a_last_digit) / (2_u64.pow((a_last_digit ^ 1) as u32))))
+                    % 8
+            {
+                // a_last_digit solves the above equation, so add this digit to the number and check
+                // if digits can be found after this that leads to a complete number. If so, return
+                // true. If output_index == 0, then this was the last digit and a complete number
+                // has just been found, so also return true in that case.
+                a_octal_digits.push(a_last_digit);
+                if output_index == 0
+                    || get_a_octal_digits(a_octal_digits, output, output_index - 1, 0)
+                {
+                    return true;
+                }
+
+                // If the inner get_a_octal_digits returned false, then there is no set of a digits
+                // after this digit that completes the output sequence correctly. So remove this
+                // digit and move on to checking the next digit.
+                a_octal_digits.pop();
+            }
+        }
+
+        false
+    }
+
+    // The initial register values do not matter. a is overwritten by whatever value causes the
+    // program to output itself, and b and c are written to before they are read. So just skip ahead
+    // to reading the program values.
+    let outputs = input
+        .lines()
+        .nth(4)
+        .expect("Input should have five lines")
+        .trim_start_matches("Program: ")
+        .split(',')
+        .map(|value| {
+            value
+                .parse()
+                .expect("Line should only have numbers after trimming and splitting")
+        })
+        .collect::<Vec<_>>();
+    let mut a_octal_digits = Vec::with_capacity(outputs.len());
+
+    get_a_octal_digits(&mut a_octal_digits, &outputs, outputs.len() - 1, 1);
+
+    // Convert the octal digits into a decimal number.
+    let register_a = a_octal_digits
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (place, digit)| {
+            acc + digit * 8_u64.pow(place as u32)
+        });
+    Solution::U64(register_a)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn example1_1() {
+        assert_eq!(
+            solve_1(
+                "\
+Register A: 10
+Register B: 0
+Register C: 0
+
+Program: 5,0,5,1,5,4"
+            ),
+            Solution::Str("0,1,2")
+        );
+    }
+    #[test]
+    fn example1_2() {
+        assert_eq!(
+            solve_1(
+                "\
+Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0"
+            ),
+            Solution::Str("4,2,5,6,7,7,7,7,3,1,0")
+        );
+    }
+    #[test]
+    fn example1_3() {
         assert_eq!(
             solve_1(
                 "\
@@ -211,4 +394,7 @@ Program: 0,1,5,4,3,0"
             Solution::Str("4,6,3,5,6,3,5,2,1,0")
         );
     }
+
+    // A test for part 2 is provided, but it violates the assumptions I had to make about my own puzzle
+    // input, so it cannot be used to test my solution.
 }
