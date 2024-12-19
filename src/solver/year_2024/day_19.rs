8019d246e837b6709e8dabfc8323502f9f444faf
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-12-19 14:43:06 +0800

    Solved 2024 19_2

--- src/solver/year_2024/day_19.rs
+++ src/solver/year_2024/day_19.rs
@@ -1,34 +1,14 @@
 use crate::solver::{Solution, Solver};
+use rustc_hash::FxHashMap;
 
 pub const SOLVER: Solver = Solver {
     year: 2024,
     day: 19,
     title: "Linen Layout",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
 fn solve_1(input: &str) -> Solution {
-    #[derive(PartialEq)]
-    enum Stripe {
-        White,
-        Blue,
-        Black,
-        Red,
-        Green,
-    }
-    impl Stripe {
-        fn new(letter: char) -> Stripe {
-            match letter {
-                'w' => Stripe::White,
-                'u' => Stripe::Blue,
-                'b' => Stripe::Black,
-                'r' => Stripe::Red,
-                'g' => Stripe::Green,
-                _ => panic!("Stripe colour is incorrect"),
-            }
-        }
-    }
-
     // Recursively check all arrangement of towels to see if the pattern can be created from the
     // available towels.
     fn can_pattern_come_from_towels(
@@ -63,6 +43,102 @@ fn solve_1(input: &str) -> Solution {
         false
     }
 
+    let (towels, pattern_iter) = get_towels_and_pattern_iter(input);
+
+    // For every pattern, check if it can be made with the available towels.
+    let mut possible_patterns = 0;
+    for pattern in pattern_iter {
+        if can_pattern_come_from_towels(&pattern, &towels, 0) {
+            possible_patterns += 1;
+        }
+    }
+    Solution::U32(possible_patterns)
+}
+
+fn solve_2(input: &str) -> Solution {
+    // Recursively check all arrangement of towels to find the total number of valid arrangements.
+    fn pattern_arrangements_from_towels(
+        pattern: &[Stripe],
+        towels: &[Vec<Stripe>],
+        arrangements_at_position: &mut FxHashMap<usize, u64>,
+        pattern_index: usize,
+    ) -> u64 {
+        // The number of arrangements possible from a given position of the pattern never changes.
+        // To avoid unnecessary recursion, remember the number of arrangements for a position the
+        // first time it is calculated, and use this remembered value if it is needed again.
+        if let Some(arrangements) = arrangements_at_position.get(&pattern_index) {
+            return *arrangements;
+        }
+
+        let mut arrangements = 0;
+        for towel in towels {
+            if towel.iter().enumerate().all(|(towel_index, towel_stripe)| {
+                pattern
+                    .get(pattern_index + towel_index)
+                    .is_some_and(|pattern_stripe| *pattern_stripe == *towel_stripe)
+            }) {
+                let new_pattern_index = pattern_index + towel.len();
+                // If the new pattern index matches the length of the pattern, the pattern has been
+                // matched perfectly, so add 1.
+                if new_pattern_index == pattern.len() {
+                    arrangements += 1;
+                } else {
+                    // Otherwise, recursively check more towels to see how many valid arrangements
+                    // can be made from here, and add them to the total.
+                    arrangements += pattern_arrangements_from_towels(
+                        pattern,
+                        towels,
+                        arrangements_at_position,
+                        new_pattern_index,
+                    );
+                }
+            }
+        }
+
+        // Remember the number of arrangements at this position, and return it.
+        arrangements_at_position.insert(pattern_index, arrangements);
+        arrangements
+    }
+
+    let (towels, pattern_iter) = get_towels_and_pattern_iter(input);
+
+    let mut possible_arrangements = 0;
+    for pattern in pattern_iter {
+        // Provide an empty hash map to the first function call so it can be used during the
+        // recursion to cache values.
+        possible_arrangements +=
+            pattern_arrangements_from_towels(&pattern, &towels, &mut FxHashMap::default(), 0);
+    }
+    Solution::U64(possible_arrangements)
+}
+
+#[derive(PartialEq)]
+enum Stripe {
+    White,
+    Blue,
+    Black,
+    Red,
+    Green,
+}
+impl Stripe {
+    fn new(letter: char) -> Stripe {
+        match letter {
+            'w' => Stripe::White,
+            'u' => Stripe::Blue,
+            'b' => Stripe::Black,
+            'r' => Stripe::Red,
+            'g' => Stripe::Green,
+            _ => panic!("Stripe colour is incorrect"),
+        }
+    }
+}
+
+fn get_towels_and_pattern_iter(
+    input: &str,
+) -> (
+    Vec<Vec<Stripe>>,
+    impl Iterator<Item = Vec<Stripe>> + use<'_>,
+) {
     let mut towels = Vec::new();
     let mut line_iter = input.lines();
     let towel_iter = line_iter
@@ -76,16 +152,11 @@ fn solve_1(input: &str) -> Solution {
         towels.push(towel);
     }
 
-    // For every remaining pattern, check if it can be made with the available towels. Skip the
-    // first line of line_iter as it is the blank line between the towels and the patterns.
-    let mut possible_patterns = 0;
-    for line in line_iter.skip(1) {
-        let pattern = line.chars().map(Stripe::new).collect::<Vec<_>>();
-        if can_pattern_come_from_towels(&pattern, &towels, 0) {
-            possible_patterns += 1;
-        }
-    }
-    Solution::U32(possible_patterns)
+    let pattern_iter = line_iter
+        .skip(1)
+        .map(|line| line.chars().map(Stripe::new).collect::<Vec<_>>());
+
+    (towels, pattern_iter)
 }
 
 #[cfg(test)]
@@ -109,6 +180,26 @@ brgr
 bbrgwb"
             ),
             Solution::U8(6)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"
+            ),
+            Solution::U8(16)
         );
     }
 }
