8f92b69792b2e2f5dbd9d122312cfff9b7467c0f
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-06-16 21:20:40 +0800

    Solved 2023 06_2

--- src/solver/year_2023/day_06.rs
+++ src/solver/year_2023/day_06.rs
@@ -4,32 +4,10 @@ pub const SOLVER: AdventOfCode = AdventOfCode {
     year: 2023,
     day: 6,
     title: "Wait For It",
-    part_solvers: &[solve_1],
+    part_solvers: &[solve_1, solve_2],
 };
 
-fn solve_1(input: &str) -> Solution {
-    let mut line_iter = input.lines();
-    let times = line_iter
-        .next()
-        .expect("line_iter should have first line")
-        .strip_prefix("Time:")
-        .expect("Line should start with \"Time:\"")
-        .split_ascii_whitespace()
-        .map(|time_str| time_str.parse().expect("time_str should be a valid number"))
-        .collect::<Vec<u32>>();
-    let distances = line_iter
-        .next()
-        .expect("line_iter should have second line")
-        .strip_prefix("Distance:")
-        .expect("Line should start with \"Distance:\"")
-        .split_ascii_whitespace()
-        .map(|distance_str| {
-            distance_str
-                .parse()
-                .expect("distance_str should be a valid number")
-        })
-        .collect::<Vec<u32>>();
-
+fn get_ways_to_win(time: u64, distance: u64) -> u64 {
     // let:
     // race time (milliseconds) = t
     // distance record (millimeters) = d
@@ -72,28 +50,74 @@ fn solve_1(input: &str) -> Solution {
     // upper limits of the charge time always produce the same distance, hence only one bound needs
     // to be checked.
 
+    let discriminant_isqrt = (time * time - 4 * distance).isqrt();
+    // t - isqrt(t^2 - 4d) / 2
+    let first_winning_charge_time = (time - discriminant_isqrt).div_ceil(2);
+    // t + isqrt(t^2 - 4d) / 2 (Note by coincidence this matches the definition for the midpoint
+    // between two values; this lint is intentionally disabled as using the midpoint function
+    // obscures the intent)
+    #[allow(clippy::manual_midpoint)]
+    let last_winning_charge_time = (time + discriminant_isqrt) / 2;
+
+    // Check if charge time exactly lines up with distance record, subtract 2 if so.
+    if first_winning_charge_time * (time - first_winning_charge_time) == distance {
+        last_winning_charge_time - first_winning_charge_time - 1
+    } else {
+        last_winning_charge_time - first_winning_charge_time + 1
+    }
+}
+
+fn solve_1(input: &str) -> Solution {
+    let mut line_iter = input.lines();
+    let times = line_iter
+        .next()
+        .expect("line_iter should have first line")
+        .strip_prefix("Time:")
+        .expect("Line should start with \"Time:\"")
+        .split_ascii_whitespace()
+        .map(|time_str| time_str.parse().expect("time_str should be a valid number"))
+        .collect::<Vec<_>>();
+    let distances = line_iter
+        .next()
+        .expect("line_iter should have second line")
+        .strip_prefix("Distance:")
+        .expect("Line should start with \"Distance:\"")
+        .split_ascii_whitespace()
+        .map(|distance_str| {
+            distance_str
+                .parse()
+                .expect("distance_str should be a valid number")
+        })
+        .collect::<Vec<_>>();
+
     let mut ways_to_win_product = 1;
     for (time, distance) in times.into_iter().zip(distances) {
-        let discriminant_isqrt = (time * time - 4 * distance).isqrt();
-        // t - isqrt(t^2 - 4d) / 2
-        let first_winning_charge_time = (time - discriminant_isqrt).div_ceil(2);
-        // t + isqrt(t^2 - 4d) / 2 (Note by coincidence this matches the definition for the midpoint
-        // between two values; this lint is intentionally disabled as using the midpoint function
-        // obscures the intent)
-        #[allow(clippy::manual_midpoint)]
-        let last_winning_charge_time = (time + discriminant_isqrt) / 2;
-
-        // Check if charge time exactly lines up with distance record, subtract 2 if so.
-        let ways_to_win =
-            if first_winning_charge_time * (time - first_winning_charge_time) == distance {
-                last_winning_charge_time - first_winning_charge_time - 1
-            } else {
-                last_winning_charge_time - first_winning_charge_time + 1
-            };
-        ways_to_win_product *= ways_to_win;
+        ways_to_win_product *= get_ways_to_win(time, distance);
     }
 
-    Solution::U32(ways_to_win_product)
+    Solution::U64(ways_to_win_product)
+}
+
+fn solve_2(input: &str) -> Solution {
+    let mut line_iter = input.lines();
+    let time = line_iter
+        .next()
+        .expect("line_iter should have first line")
+        .chars()
+        .filter(char::is_ascii_digit)
+        .collect::<String>()
+        .parse()
+        .expect("time string should be a valid number after filtering non-digit characters");
+    let distance = line_iter
+        .next()
+        .expect("line_iter should have second line")
+        .chars()
+        .filter(char::is_ascii_digit)
+        .collect::<String>()
+        .parse()
+        .expect("distance string should be a valid number after filtering non-digit characters");
+
+    Solution::U64(get_ways_to_win(time, distance))
 }
 
 #[cfg(test)]
@@ -109,6 +133,18 @@ Time:      7  15   30
 Distance:  9  40  200"
             ),
             Solution::U16(288)
+        );
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            solve_2(
+                "\
+Time:      7  15   30
+Distance:  9  40  200"
+            ),
+            Solution::U32(71503)
         );
     }
 }
